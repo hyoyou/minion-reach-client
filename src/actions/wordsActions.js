e9ad2d15b3a7a3bc8efb1af4f3d9bf6ddc8aee74
Heather You <heatherhyou@gmail.com>
2018-09-10 15:21:51 -0400

    Receive only 1 word back from API, work on other features on feature branch

--- src/actions/wordsActions.js
+++ src/actions/wordsActions.js
@@ -6,13 +6,13 @@ const APIURL = `http://localhost:3001/api/words`;
 export const fetchWord = (difficulty) => {
     return function(dispatch) {
         return fetch(`${APIURL}/${difficulty}`)
-        .then(response => response.json())
+        .then(response => response.text())
         .then(result => {
-            let secretWord = result[Math.floor(Math.random()*result.length)];
+            let secretWord = result.toUpperCase();
             
             dispatch({
                 type: types.FETCH_WORD,
-                payload: secretWord.toUpperCase()
+                payload: secretWord
             })
         })
     }
